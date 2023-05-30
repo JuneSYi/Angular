b04967fc9f9b6a25317a59042a62be3f956f71f8
Michelle Yi <junetheyi@gmail.com>
2023-05-29 23:16:01 -0700

    my first angular app

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);
+  }
+
+}
